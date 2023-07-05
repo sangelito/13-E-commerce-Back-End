3f6168e97d0368ce9c41220606b782fe551dc4f3
sangelito <129316135+sangelito@users.noreply.github.com>
2023-07-05 10:49:52 -0500

    product find all

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+    ]
+})
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 // get one product
