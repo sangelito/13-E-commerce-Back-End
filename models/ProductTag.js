3c1c1ebb016d5b1e544899827104e00fdf28c221
sangelito <129316135+sangelito@users.noreply.github.com>
2023-07-05 09:46:20 -0500

    id, product, tag columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,10 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // id column 
-
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+  },
     // product_id column 
-    
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: "product",
+          key: "id"
+      }
+  },
     // tag_id column 
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: "tag",
+          key: "id"
+      }
+  }
   },
   {
     sequelize,
